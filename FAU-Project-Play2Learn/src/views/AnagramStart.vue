ba81efe6fd145f5d9433edb82d1cca732115f892
Robert Sleppy <rassleppy@gmail.com>
2025-12-07 21:04:12 -0500

    Add AnagramStart UI with input, validation, and basic structure

--- FAU-Project-Play2Learn/src/views/AnagramStart.vue
+++ FAU-Project-Play2Learn/src/views/AnagramStart.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
-    <h1>Anagram Start Page</h1>
+    <h1>Anagram Hunt</h1>
+    <label> 
+        Word Length
+        <input type="number" min="3" max="10" v-model.number="wordLength" />
+    </label>
+    <!-- instructions I believe will be listed here-->
+    <button
+      @click="startGame"
+      :disabled="!isValidLength"
+    >
+      Play!
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AnagramStart',
+    data() {
+        return {
+        wordLength: 5,
+        };
+    },
+    computed: {
+        isValidLength() {
+        return this.wordLength >= 3 && this.wordLength <= 10;
+        },
+    },
+    methods: {
+        startGame() {},
+    },
 };
 </script>
